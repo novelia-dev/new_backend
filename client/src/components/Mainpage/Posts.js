522587b42158b135ea318ed2ffc682083b519f9b
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-03-04 05:41:35 +0900

    mainpage + Navbar 수정

--- client/src/components/Mainpage/Posts.js
+++ client/src/components/Mainpage/Posts.js
@@ -13,7 +13,9 @@ const Posts = ({ posts, loading }) => {
       {loading && <div> loading... </div>}
       
         {posts.map((post) => (
-          <div className="contanier-fluid" key={post.id}>
+          <table>
+            <td>
+            <div className="contanier-fluid" key={post.id}>
             <div class="row">
               <div class="col-12 mt-3">
                  <div class="card-horizontal" onClick={movetodetail}>
@@ -55,6 +57,52 @@ const Posts = ({ posts, loading }) => {
               </div>
             </div>
           </div>
+            </td>
+            <td>
+            <div className="contanier-fluid" key={post.id}>
+            <div class="row">
+              <div class="col-12 mt-3">
+                 <div class="card-horizontal" onClick={movetodetail}>
+                    <div class="img-square-wrapper" style={{marginTop:"25px"}}>
+                      <img src={image} alt="사진" />
+                    </div>
+                      <div class="card-body" >
+                        <h4 class="card-title">재벌집 막내아들</h4>
+                        <p class="card-text">
+                          <h6>산경</h6>
+                          <h6>현대 판타지 &nbsp; D-14</h6>
+                          <h6>10/30 &nbsp; 10/10</h6>
+                          <h6>태그</h6>
+                          <table>
+                            <tr>
+                              <button>선택태그</button>
+                              <button>선택태그</button>
+                              <button>선택태그</button>
+                            </tr>
+                            <tr>
+                            <button>선호태그</button>
+                            <button>선호태그</button>
+                            <button>선호태그</button>
+                            </tr>
+                            <tr>
+                            <button>선호태그</button>
+                            <button>선호태그</button>
+                            <button>선호태그</button>
+                            </tr>
+                            <tr>
+                            <button>선호태그</button>
+                            <button>선호태그</button>
+                            <button>선호태그</button>
+                            </tr>
+                          </table>
+                        </p>
+                      </div>
+                 </div>
+              </div>
+            </div>
+          </div>
+            </td>
+          </table>
         ))}
    
     </>
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -21,28 +21,10 @@ function Navbar(){
            <td><input type="text" placeholder="  #해쉬태그로 검색해보세요" style={{width: "588px",height: "30px", marginTop:"30px",marginLeft:"318.1px",backgroundColor:'#FFFFFF',borderRadius:"50px",}}></input></td>
            <td>
             <img className="Bell" alt="Bell" style={{width:"18px", height:"25px",marginTop:"30px",marginLeft:"324px"}}src={Bell} />
-            <img className="NewText" alt="NewText" style={{width:"24px", height:"23px",marginTop:"30px",marginLeft:"363px"}}src={NewText}/>
-            <div id="My">
-              <nav id = "nav" style={{width:"30px"}}>
-                 <img className="Mypage" alt="Mypage" style={{width:"24px", height:"23px",marginTop:"30px",marginLeft:"399px"}}src={Mypage} onClick={movetomypage}></img>
-            <li id="dropDown">
-              <h4>@@@님</h4>
-              <h5>5000 point  포인트샵</h5>
-              <h5>독자 페이지</h5>
-              <h5>스크랩한 작품</h5>
-              <h5>피드백 참여 작품</h5>
-              <br />
-              <h5>작가 페이지</h5>
-              <h5>새글등록</h5>
-              <h5>피드백 진행중인 작품</h5>
-              <h5>피드백 완료된 작품</h5>
-              <h5>쿠폰함</h5>
-              <br />
-              <h5>기본정보</h5>
-              <h5>기본정보 및 선호/금지태그 수정</h5>
-            </li>
-            </nav>
-            </div>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <img className="NewText" alt="NewText" style={{width:"24px", height:"23px"}}src={NewText}/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <img className="Mypage" alt="Mypage" style={{width:"24px", height:"23px"}}src={Mypage} onClick={movetomypage}></img>
            </td>
            </tr>
            </table>
